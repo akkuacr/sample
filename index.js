3b22ae4da55598f9a3398a00deb5eae6ad8c3ebf
Akash Chand <chandakash958@gmail.com>
2024-09-17 09:36:37 +0530

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const age = 24;
+const name = 'Akash Chand'
+const dob = '01/01/2024'
+
+function sayName(){
+    console.log('My Name is '+ name);
+    
+}
+
+function isAbove18(){
+    return (age>18);
+}
+
+
+console.log(isAbove18());
